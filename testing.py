23753efbc6a1e2b22d16e2b004df7b86b989c4f9
danielavm02 <danielavargas@ufm.edu>
2022-05-10 10:29:29 -0600

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,9 +1,11 @@
 from lists import maximo
 from lists import list_merge
 from bruteforce import sum_numbers
+from bruteforce import pin_unlock
 
 A=[1,3,5,7,9]
 B=[2,4,6,8,10]
 maximo.maximo(A)
 print(list_merge.juntar(A,B))
 sum_numbers.suma(5)
+print(pin_unlock.unlock("2812"))
