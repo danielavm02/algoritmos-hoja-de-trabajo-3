3cb7551c0e520c7cfb9a12b916252fd65ea42388
danielavm02 <danielavargas@ufm.edu>
2022-05-10 09:51:30 -0600

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,7 +1,9 @@
 from lists import maximo
 from lists import list_merge
+from bruteforce import sum_numbers
 
 A=[1,3,5,7,9]
 B=[2,4,6,8,10]
 maximo.maximo(A)
 print(list_merge.juntar(A,B))
+sum_numbers.suma(5)
