f842e7ba112150c61e2bb5928ed26ab402d43a26
danielavm02 <danielavargas@ufm.edu>
2022-05-10 10:46:22 -0600

    Update testing.py

--- testing.py
+++ testing.py
@@ -5,6 +5,7 @@ from bruteforce import pin_unlock
 from bruteforce import divisores_n
 from recursion import sum_of_n
 from recursion import fibonacci
+from recursion import factorial
 
 A=[1,3,5,7,9]
 B=[2,4,6,8,10]
@@ -12,6 +13,7 @@ B=[2,4,6,8,10]
 # print(list_merge.juntar(A,B))
 # sum_numbers.suma(5)
 # print(pin_unlock.unlock("2812"))
-print(" Divisores: " + str(divisores_n.divisores(9)))
-print(sum_of_n.sum(30))
-print(fibonacci.fibonacci(10))
+# print(" Divisores: " + str(divisores_n.divisores(9)))
+# print(sum_of_n.sum(30))
+# print(fibonacci.fibonacci(10))
+print (factorial.fact(5))
