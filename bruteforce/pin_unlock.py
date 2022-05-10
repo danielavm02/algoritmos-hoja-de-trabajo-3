74722fc6d11aefac2fb8fdafa1afac70b38fbfbc
danielavm02 <danielavargas@ufm.edu>
2022-05-10 10:29:24 -0600

    Update pin_unlock.py

--- bruteforce/pin_unlock.py
+++ bruteforce/pin_unlock.py
@@ -6,10 +6,9 @@ def unlock(pin_param):
             for k in range(10):
                 for l in range (10):
                     guess = str(i)+str(j)+str(k)+str(l)
-                    print(guess)
                     sleep(0.001)
                     if guess==pin:
                         result = "El pin es: " + guess
                         return result 
-print(unlock("2812"))
+
 
