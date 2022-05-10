8e9adecacb3ff2fafeec65b077feb304c5f4c0e3
danielavm02 <danielavargas@ufm.edu>
2022-05-10 10:36:08 -0600

    Update testing.py

--- testing.py
+++ testing.py
@@ -2,10 +2,12 @@ from lists import maximo
 from lists import list_merge
 from bruteforce import sum_numbers
 from bruteforce import pin_unlock
+from bruteforce import divisores_n
 
 A=[1,3,5,7,9]
 B=[2,4,6,8,10]
-maximo.maximo(A)
-print(list_merge.juntar(A,B))
-sum_numbers.suma(5)
-print(pin_unlock.unlock("2812"))
+# maximo.maximo(A)
+# print(list_merge.juntar(A,B))
+# sum_numbers.suma(5)
+# print(pin_unlock.unlock("2812"))
+print(" Divisores: " + str(divisores_n.divisores(9)))
