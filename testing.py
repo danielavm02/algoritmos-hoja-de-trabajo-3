985fa4ac78be212368d11ac78121c43b00cf36e4
danielavm02 <danielavargas@ufm.edu>
2022-05-10 11:07:44 -0600

    Update testing.py

--- testing.py
+++ testing.py
@@ -9,6 +9,7 @@ from recursion import factorial
 from recursion import countdown
 from sorting import bubble_sort
 from sorting import selection_sort
+from sorting import bubble_opt
 
 A=[1,3,5,7,9]
 B=[2,4,6,8,10]
@@ -21,6 +22,7 @@ B=[2,4,6,8,10]
 # print(fibonacci.fibonacci(10))
 #print (factorial.fact(5))
 # countdown.cuenta(10)
-# list=[4,1,9,8]
+list=[4,1,9,8,6,10]
 # print(bubble_sort.bubble_sort(list))
 print(selection_sort.selection_sort(list))
+print(bubble_opt.bubble_sort_opt(list))
